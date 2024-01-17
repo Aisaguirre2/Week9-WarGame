52092f18fb19e2adb01133d27a62cab142f3ee2f
Aisaguirre2 <147682337+Aisaguirre2@users.noreply.github.com>
2024-01-17 13:38:53 -0700

    first commit

---
+++ index_test.js
@@ -0,0 +1,10 @@
+var expect = chai.expect;
+
+describe('MyFunctions', function() {
+    describe('#freshDeck', function() {
+        it('should produce an array of 52 Cards', function() {
+            const testDeck = new Deck();
+            expect(testDeck.cards.length).to.equal(52);
+        });
+    });
+});
